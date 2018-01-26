55a1464bb51450f8b565f2cc0cc7fc53fff451c8
Kendrick Shen <kshen6@stanford.edu>
2018-01-25 18:36:28 -0800

    individual CSS for each

--- assets/js/src/Company.jsx
+++ assets/js/src/Company.jsx
@@ -4,9 +4,9 @@ class Company extends Component {
     render () {
         return (
             <div>
-                <div className="container body-sect">
+                <div className="container">
                     <div className="row">
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2><b>Company</b></h2>
                         </div>
                     </div>
@@ -18,130 +18,106 @@ class Company extends Component {
                             and we are here to put the cutting edge of data analytics into your hands.</p>
                     </div>
 
-                    <div className="row member-row">
+                    <div className="row" id="member-row">
 
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2>Michelle Wu</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/michelle-wu.jpg" alt="Michelle Wu" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford MBA Candidate '18<br/>
                                 Prior: Longitude Capital, Wuxi Ventures, Novartis
                             </p>
+                            <div className="btn-group" role="group">
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/michelle-wu-gsb/"><b>in</b></a></button>
+                            </div>
                         </div>
 
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2>Andy Wardle, MD</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/andy-wardle.jpg" alt="Andy Wardle" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford MBA Candidate '19<br/>
                                 Prior: Boston Consulting Group
                             </p>
+                            <div className="btn-group" role="group">
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/andrew-wardle-6a108b68/"><b>in</b></a></button>
+                            </div>
                         </div>
 
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2>Eric Loreaux</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/eric-loreaux.jpg" alt="Eric Loreaux" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford MS in Bioengineering Candidate '19
                             </p>
+                            <div className="btn-group" role="group">
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/eric-loreaux-011672b2/"><b>in</b></a></button>
+                            </div>
                         </div>
 
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2>Charlie Reis</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/charlie-reis.jpg" alt="Charlie Reis" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford MBA Candidate '19<br/>
                                 Prior: Abbott Regulatory Affairs
                             </p>
-                        </div>
-                    </div>
-
-                    <div className="row social-row">
-                        <div className="col-sm text-center">
                             <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/michelle-wu-gsb/">LinkedIn</a></button>
-                            </div>
-                        </div>
-
-                        <div className="col-sm text-center">
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/andrew-wardle-6a108b68/">LinkedIn</a></button>
-                            </div>
-                        </div>
-
-                        <div className="col-sm text-center">
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/eric-loreaux-011672b2/">LinkedIn</a></button>
-                            </div>
-                        </div>
-
-                        <div className="col-sm text-center">
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/charlesrreis/">LinkedIn</a></button>
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/charlesrreis/"><b>in</b></a></button>
                             </div>
                         </div>
                     </div>
 
-                    <div className="row member-row">
-                        <div className="col-sm text-center">
+                    <div className="row" id="member-row">
+                        <div className="col-sm">
                             <h2>Abhiram Rao</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/abhiram-rao.jpg" alt="Abhiram Rao" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford PhD in Bioengineering '20
                             </p>
+                            <div className="btn-group" role="group">
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/raoabhiram/"><b>in</b></a></button>
+                            </div>
                         </div>
                 
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2>Kendrick Shen</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/kendrick-shen.jpg" alt="Kendrick Shen" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford BS in Symbolic Systems Candidate '21
                             </p>
+                            <div className="btn-group" role="group">
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/kendrickshen/"><b>in</b></a></button>
+                            </div>
                         </div>
 
-                        <div className="col-sm text-center">
+                        <div className="col-sm">
                             <h2>Ashwin Ramaswami</h2>
-                            <div className="member-img">
+                            <div>
                                 <img src="assets/img/ashwin-ramaswami.jpg" alt="Ashwin Ramaswami" height="150em" className="rounded-circle"/>
                             </div>
                             <p>
                                 Stanford BS in Computer Science Candidate '21<br/>
                                 Microsoft Student Partner
                             </p>
-                        </div>
-                    </div>
-
-                    <div className="row social-row">
-                        <div className="col-sm text-center">
                             <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/raoabhiram/">LinkedIn</a></button>
-                            </div>
-                        </div>
-
-                        <div className="col-sm text-center">
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/kendrickshen/">LinkedIn</a></button>
-                            </div>
-                        </div>
-
-                        <div className="col-sm text-center">
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/ashwin-r/">LinkedIn</a></button>
+                                <button type="button" className="btn"><a id="social" href="https://www.linkedin.com/in/ashwin-r/"><b>in</b></a></button>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         );
--- assets/js/src/Main.jsx
+++ assets/js/src/Main.jsx
@@ -8,21 +8,25 @@ import Home from "./Home.jsx";
 import Vision from "./Vision.jsx";
 import Company from "./Company.jsx";
 
+import "./Main.scss";
+import "./Home.scss";
+import "./Vision.scss";
+import "./Company.scss";
+
 class Main extends Component {
     render () {
         return (
             <HashRouter>
                 <div>
 
-                    <nav className="navbar navbar-top text-uppercase">
-                        <a className="navbar-brand" href="index.html">
+                    <nav className="navbar" id="navbar-top">
+                        <NavLink exact to="/" className="navbar-brand">
                             <img src="./assets/img/Logo.png" height="50" className="d-inline-block align-top" alt="Nova Approval" />
-                        </a>
+                        </NavLink>
                         <div className="navbar-text navbar-right">
-                            <NavLink exact to="/" className="btn" role="button">Home</NavLink>
                             <NavLink to="/Vision" className="btn" role="button">Vision</NavLink>
-                            <NavLink to="Company" className="btn" role="button">Company</NavLink>
-                            <a href="http://demo.nova-approval.com" className="btn" id="btn-demo" role="button">Launch Demo</a>
+                            <NavLink to="/Company" className="btn" role="button">Company</NavLink>
+                            <a href="http://demo.nova-approval.com" className="btn" id="btn-demo" role="button">Demo</a>
                         </div>
                     </nav>
 
@@ -32,11 +36,15 @@ class Main extends Component {
                         <Route path="/Company" component={Company}/>
                     </div>
 
-                    <nav className="navbar navbar-bottom">
-                        <a className="navbar-brand" href="index.html">
+                    <nav className="navbar" id="navbar-bottom">
+                        <NavLink exact to="/" className="navbar-brand">
                             <img src="./assets/img/Logo.png" height="50" className="d-inline-block align-top" alt="Nova Approval" />
-                        </a>
-                        <p>
+                        </NavLink>
+                        <div>
+                            <a className="btn fa fa-twitter" href="https://twitter.com/nova_approval"></a>
+                            <a className="btn fa fa-linkedin" href="https://www.linkedin.com/company/nova-approval/"></a>
+                        </div>
+                        <p className="navbar-text">
                             Address<br />
                             Knight Management Center<br />
                             655 Knight Way<br />
